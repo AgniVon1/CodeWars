09c9ad6a528d24bb35a9035810ab28a74410f7c5
Agni <itron363@gmail.com>
2022-10-08 04:35:52 +0300

    Fix: The first solution was wrong

--- Kata8/Count of positives  sum of negatives.js
+++ Kata8/Count of positives  sum of negatives.js
@@ -1,9 +1,17 @@
 //codewars.com/kata/576bb71bbbcf0951d5000044/train/javascript
 function countPositivesSumNegatives(input) {
-    var answer =[0,0];
-    for(let buff of input){
-        answer[(buff > 0)? 0:1] +=buff;
+    if  (input === null || input.length === 0){
+        return [];
     }
-    return answer;
+    else{
+        var answer =[0,0];
+        for(let buff of input){
+            if (buff > 0)
+            {answer[0]+=1;}
+            else
+            {answer[1] +=buff;}
+        }
+        return answer;
+    }
 }
 console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]))
